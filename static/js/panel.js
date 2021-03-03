6bd830c612b898b3ac7725ce2271b21d42f1c2b9
Mohit Rathod <57721715+mohitrathod7@users.noreply.github.com>
2021-03-04 04:03:55 +0530

    Update panel.js

--- static/js/panel.js
+++ static/js/panel.js
@@ -1,9 +1,24 @@
-/* Set the width of the sidebar to 250px (show it) */
+/* Display Nav */
 function openNav() {
     document.getElementById("mySidepanel").style.width = "250px";
 }
 
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-}
+const sidePanel = gsap.timeline({ defaults: {duration: 1 }});
+
+$('.fa-bars').click(function(){
+    sidePanel
+        .from('#mySidepanel a', { ease: 'power2.in', duration: 0.4, x: -300, stagger: 0.15 })
+})
+
+
+$('.closebtn').click(function(){        
+    function hidepanel() {
+        document.getElementById("mySidepanel").style.width = "0";
+    }
+
+    sidePanel
+        .to('#mySidepanel a', { ease: 'power2.in', duration: 0.4, x: -300, stagger: 0.15 })
+        .to('#mySidepanel a', { ease: 'power2.in', duration: 0.4, x: 0 })
+    
+    setTimeout(hidepanel, 1000);
+})
