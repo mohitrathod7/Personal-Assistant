516e9549bf0b615e9d78c1613cc000db8a32afea
Mohit Rathod <57721715+mohitrathod7@users.noreply.github.com>
2021-03-09 06:53:32 +0530

    Update password.js

--- static/js/password.js
+++ static/js/password.js
@@ -1,5 +1,6 @@
-function passwordVisibility(){
-    var password = document.getElementById("password");
+function passwordVisibility(clicked_id){
+    var password = document.getElementById(clicked_id);
+
     var show_password = document.getElementById("show-password");
     var hide_password = document.getElementById("hide-password");
 
@@ -12,5 +13,28 @@ function passwordVisibility(){
         password.type = "password";
         hide_password.style.display = "none";
         show_password.style.display = "flex";
+    }
+}
+
+
+function passwordVisibilityMultiple(clicked_id){
+    clicked_id.forEach(visible);
+
+    function visible(item){
+        var password = document.getElementById(item);
+
+        var show_password = document.getElementById("show-password");
+        var hide_password = document.getElementById("hide-password");
+
+        if (password.type == "password"){
+            password.type = "text";
+            hide_password.style.display = "flex";
+            show_password.style.display = "none";
+        }
+        else{
+            password.type = "password";
+            hide_password.style.display = "none";
+            show_password.style.display = "flex";
+        }
     }
 }
